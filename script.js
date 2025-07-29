efc148c22e87ef07028a14e399b7557b8f6fe293
irfan ansari <imirfanansari0001@gmail.com>
2025-07-29 18:26:33 +0530

    Add initial HTML and JavaScript files with basic

---
+++ script.js
@@ -0,0 +1,21 @@
+// // let a = 10; //outside
+// // {
+// //     let a = 20; //inside
+// //     console.log("inside",a);
+// // }
+
+// // console.log("outside", a);
+
+
+//     // let a = 15;
+//     // let b = a;
+//     // // a = a15;
+    
+// let number = 151215161516212n;
+// number+5n;
+
+
+//  concadenation
+let a = "5"+ 5 ;
+
+console.log("concadenation" , a);
