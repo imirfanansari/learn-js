4a036a925ced5912fea7efe7f9e5c6ac0f2d6d11
irfan ansari <imirfanansari0001@gmail.com>
2025-07-29 18:26:42 +0530

    Add initial HTML and JavaScript files with basic structure and concatenation example

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-// // let a = 10; //outside
-// // {
-// //     let a = 20; //inside
-// //     console.log("inside",a);
-// // }
+// // // let a = 10; //outside
+// // // {
+// // //     let a = 20; //inside
+// // //     console.log("inside",a);
+// // // }
 
-// // console.log("outside", a);
+// // // console.log("outside", a);
 
 
-//     // let a = 15;
-//     // let b = a;
-//     // // a = a15;
+// //     // let a = 15;
+// //     // let b = a;
+// //     // // a = a15;
     
-// let number = 151215161516212n;
-// number+5n;
+// // let number = 151215161516212n;
+// // number+5n;
 
 
-//  concadenation
-let a = "5"+ 5 ;
+// //  concadenation
+// let a = "5"+ 5 ;
 
-console.log("concadenation" , a);
+// console.log("concatenation", a);
+let a = 2;
+let b = 3;
+console.log(a**b);
